0741a1dbb0dd081e133b36ea25b26fb773b3fe8e
Rajesh <tcscrajesh@gmail.com>
2020-08-15 01:43:42 -0700

    initial commit

---
+++ QuantsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuantsFeed
+//
+//  Created by Rajesh on 15/08/20.
+//  Copyright © 2020 Rajesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
